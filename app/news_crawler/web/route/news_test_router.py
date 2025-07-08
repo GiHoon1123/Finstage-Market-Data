973693154dcd45eb135b271ef6186d4b11c2253c
JangDeukchun <rlaejrqo465@naver.com>
2025-07-08 18:56:53 +0900

    test: 윈도우에서 테스트 커밋

--- app/news_crawler/web/route/news_test_router.py
+++ app/news_crawler/web/route/news_test_router.py
@@ -1,10 +1,10 @@
 from fastapi import APIRouter
-from app.news_crawler.service.yahoo_company_news_crawler import YahooCompanyNewsCrawler
+from app.news_crawler.service.yahoo_news_crawler import YahooNewsCrawler
 
 router = APIRouter(prefix="/test/news", tags=["Test News Crawler"])
 
 @router.post("/{symbol}")
 def crawl_and_save_news(symbol: str):
-    crawler = YahooCompanyNewsCrawler(symbol)
-    crawler.save_all()
+    crawler = YahooNewsCrawler(symbol)
+    crawler.process_all()
     return {"message": f"{symbol} 뉴스 저장 완료"}
